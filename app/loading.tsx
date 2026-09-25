c6382aa10546aa29e2fdd6c4292adfca70cb204f
webdesign-only <iamsakshamlamsal@gmail.com>
2026-09-25 21:20:21 +0545

    Use new NEPKITS wordmark in loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,11 @@
 import './globals.css';
+import NepkitsBrand from '../components/nepkits-brand';
 
 export default function Loading(){
   return <main className='site-loader' aria-label='Loading NEPKITS HUB'>
     <div className='loader-noise'/>
     <div className='loader-inner'>
-      <div className='loader-logo-frame'>
-        <img src='/nepkits-logo.webp' alt='NEPKITS HUB'/>
-      </div>
+      <div className='loader-logo-frame'><NepkitsBrand variant='loader'/></div>
       <div className='loader-kicker'>NEPKITS HUB • FOOTBALL MARKETPLACE</div>
       <h1>READY FOR<br/><em>MATCHDAY.</em></h1>
       <p>Loading jerseys, clubs and World Cup collections…</p>
