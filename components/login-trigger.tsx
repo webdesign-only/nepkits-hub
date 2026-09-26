b509202023f81582c319e06610f12f62646623b9
webdesign-only <iamsakshamlamsal@gmail.com>
2026-09-26 09:37:52 +0545

    Remove legacy login trigger

--- components/login-trigger.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-export default function LoginTrigger(){
- return <button type='button' className='market-account-button' onClick={()=>window.dispatchEvent(new Event('nepkits:login'))}><span>◯</span><b>Account</b></button>
-}
